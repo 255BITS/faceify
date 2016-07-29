71c15fabde4c54600c4a1133d46fae20ca34b790
Martyn Garcia <martyn@255bits.com>
2016-07-29 15:16:57 -0600

    [feature] facification ++

--- facify.py
+++ facify.py
@@ -4,6 +4,18 @@ import os.path
 import numpy as np
 import cv,cv2
 
+import dlib
+import argparse
+
+parser = argparse.ArgumentParser(description='Runs the GAN.')
+
+parser.add_argument('--directory', type=str)
+parser.add_argument('--output', type=str)
+args = parser.parse_args()
+DIR = args.directory
+#DIR = "../data/celeb/"
+#DIR = "/home/martyn/Downloads/imdb/imdb/81/"
+
 def Distance(p1,p2):
   dx = p2[0] - p1[0]
   dy = p2[1] - p1[1]
@@ -35,18 +47,22 @@ def CropFace(image, eye_left=(0,0), eye_right=(0,0), offset_pct=(0.2,0.2), dest_
   # get the direction
   eye_direction = (eye_right[0] - eye_left[0], eye_right[1] - eye_left[1])
   # calc rotation angle in radians
-  rotation = -math.atan2(float(eye_direction[1]),float(eye_direction[0]))
-  # distance between them
   dist = Distance(eye_left, eye_right)
   # calculate the reference eye-width
   reference = dest_sz[0] - 2.0*offset_h
   # scale factor
   scale = float(dist)/float(reference)
+
+  rotation = -math.atan2(float(eye_direction[1]),float(eye_direction[0]))
+
   # rotate original around the left eye
   image = ScaleRotateTranslate(image, center=(np.array(eye_left)+np.array(eye_right))/2, angle=rotation)
   # crop the rotated image
   crop_xy = (eye_left[0] - scale*offset_h, eye_left[1] - scale*offset_v)
   crop_size = (dest_sz[0]*scale, dest_sz[1]*scale)
+  if(crop_xy[0] < 0 or crop_xy[1] < 0 or int(crop_xy[0]+crop_size[0]) > image.size[0] or int(crop_xy[1]+crop_size[1]) > image.size[1]):
+      print("CROP OVER", crop_xy, crop_size, image.size)
+      return None
   image = image.crop((int(crop_xy[0]), int(crop_xy[1]), int(crop_xy[0]+crop_size[0]), int(crop_xy[1]+crop_size[1])))
   # resize it
   image = image.resize(dest_sz, Image.ANTIALIAS)
@@ -56,50 +72,46 @@ def CropFace(image, eye_left=(0,0), eye_right=(0,0), offset_pct=(0.2,0.2), dest_
 
 
 def get_eyes(image):
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-    eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-    img = np.asarray(image)
+    predictor = dlib.shape_predictor("./shape_predictor_68_face_landmarks.dat")
+    detector = dlib.get_frontal_face_detector()
+    img = np.array(image)
     try:
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     except:
         return None
 
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5, minSize=(20,20), maxSize=(2000,2000))
 
-    for (x,y,w,h) in faces:
-      roi_gray = gray[y:y+h, x:x+w]
-      eyes = eye_cascade.detectMultiScale(roi_gray, 1.3, 5, minSize=(20,20), maxSize=(400,400))
+    dets = detector(gray, 1)
+    faces = []
+    for k,d in enumerate(dets):
+        shape = predictor(gray, d)
+        s1 = shape.part(40)
+        s2 = shape.part(47)
+        l = np.array((int(s1.x),s1.y))
+        r = np.array((int(s2.x),s2.y))
+        threshold = 60
+        if(abs((r-l)[0])<threshold):
+          continue
+        print("appending face")
+        faces.append([l,r])
 
-      def eye_loc(eye):
-          #return eye[0]+x+(eye[2]/2), eye[1]+y+(eye[3]/2)
-          return eye[0]+x+(eye[2]/2), eye[1]+y+(eye[3]/2)
-      
-      if len(eyes) < 2:
-          return None
-      eyes = sorted(eyes, key=lambda eye: eye[3]*eye[1])
-      eyes = eyes[0:2]
-      eyes = sorted(eyes, key=lambda eye: eye[0])
-      #cv2.circle(img, eye_loc(eyes[0]), 20, 0)
-      #cv2.circle(img, eye_loc(eyes[1]), 20, 255)
-      #cv2.imshow('img',img)
-      #cv2.waitKey(0)
-      #return [eye_loc(eyes[1]), eye_loc(eyes[0])]
-      return [eye_loc(eyes[0]), eye_loc(eyes[1])]
+    print('--')
+    return faces
 
 good=0
 bad=0
-for file in os.listdir("original"):
+for file in os.listdir(DIR):
     name = file.split(".")
     if(len(name) > 1):
         name = name[-2]
     else:
         name = name[-1]
 
-    destination = "crop_faces/"+name + ".jpg"
-    if os.path.isfile(destination):
+    destination = args.output+"/"+name + ".jpg"
+    if os.path.isfile(destination.split(".")[-2]+".1"+destination.split(".")[-1]):
         print("Skip", destination)
         continue
-    input_file = "original/"+file
+    input_file = DIR+file
     try:
         image = Image.open(input_file)
     except:
@@ -107,13 +119,18 @@ for file in os.listdir("original"):
         continue
     eyes = get_eyes(image)
     if eyes:
-        offset = (0.4,0.4)
-        try:
-            CropFace(image, eye_left=eyes[0], eye_right=eyes[1], offset_pct=offset, dest_sz=(256,256)).save(destination)
-            good+=1
-        except:
-            print("Could not save", destination)
-            bad+=1
+        e_count=0
+        for e in eyes:
+            e_count+=1
+            offset = (0.4,0.51)
+            newdestination = destination.split(".")[-2] + "."+str(e_count)+"." + destination.split(".")[-1]
+            print("Cropping to "+newdestination)
+            res = CropFace(image, eye_left=e[0], eye_right=e[1], offset_pct=offset, dest_sz=(256,256))
+            if(res == None):
+                bad+=1
+            else:
+                res.save(newdestination)
+                good+=1
     else:
         bad+=1
         print("Skipping", name)
